a949200071a27699716b503f9861b71a99ce5eeb
Pavan <24794698+pavanchow@users.noreply.github.com>
2021-11-19 12:39:51 +0530

    Delete civil11-test/src directory

--- civil11-test/src/lab/siplab-simulation/scilabex1/js/valid.js
+++
@@ -1,9 +0,0 @@
-function delete_login(sno)
-{
-if(confirm("Do you want to delete the employee record"))
-{
-   this.document.admin.sno.value=sno;
-this.document.admin.act.value="delete_login";
-this.document.admin.submit();
-}
-}
